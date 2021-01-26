b0f42cfec91571ea28d1174131f8ec78beea0a07
Robyn Tyler <robynstyler@outlook.com>
2021-01-26 17:22:01 +0000

    first commit

---
+++ src/main/java/Library.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Library {
+
+    private ArrayList<Book> stock;
+    private int capacity;
+
+    public Library() {
+        this.stock = new ArrayList<Book>();
+        this.capacity = 3;
+    }
+
+    public int stockCount() {
+        return stock.size();
+    }
+
+    public void addBookToStock(Book book) {
+        if (stockCount() < this.capacity) {
+            stock.add(book);
+        }
+    }
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,45 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    private Library library;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+    private Book book4;
+
+    @Before
+    public void before(){
+        library = new Library();
+        book1 = new Book();
+        book2 = new Book();
+        book3 = new Book();
+        book4 = new Book();
+    }
+
+    @Test
+    public void stockStartsEmpty(){
+        assertEquals(0, library.stockCount());
+    }
+
+    @Test
+    public void canAddBookToStock(){
+        library.addBookToStock(book1);
+        assertEquals(1, library.stockCount());
+    }
+
+    @Test
+    public void cantAddBookToStock(){
+        library.addBookToStock(book1);
+        library.addBookToStock(book2);
+        library.addBookToStock(book3);
+        library.addBookToStock(book4);
+        assertEquals(3, library.stockCount());
+    }
+
+
+
+}
